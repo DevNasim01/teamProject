6c8ea88898b1c606aa876c4affb5bf39c2d36e1a
DevNasim01 <sknasimpc@gmail.com>
2024-04-17 20:11:59 +0530

    responsive-fix

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -7,12 +7,12 @@ const End = () => {
             <div className='bg-arrow text-[1vw] px-[2.4vw] rounded-b-lg pb-3'><i className="fa-solid fa-chevron-down"></i></div>
 
             <div className='w-full flex justify-around text-xl font-semibold mt-[4vw] mb-[2vw] px-[2vw]'>
-              <div className=' flex justify-center items-center flex-col'>
+              <div className='w-[15vw] flex justify-center items-center flex-col'>
                 <span className='h-[6vw] w-[6vw] flex items-center justify-center rounded-full bg-arrow text-[1.5vw]'><i className="fa-solid fa-eye"></i></span>
                 <p className='text-[1.2vw]'>Recently Visited</p>
               </div>
               
-              <div className=' flex justify-center items-center flex-col'>
+              <div className='w-[15vw] flex justify-center items-center flex-col'>
                 <span className='h-[6vw] w-[6vw] flex items-center justify-center rounded-full bg-arrow text-[1.5vw]'><i className="fa-solid fa-user"></i></span>
                 <p className='text-[1.2vw]'>Account making date</p>
               </div>
@@ -22,12 +22,12 @@ const End = () => {
                 <div className='bg-text h-full w-full flex items-center justify-center px-[2.5vw] py-[0.2vw] text-[1.2vw] text-black'>Actual but not varified</div>
               </div>
 
-              <div className=' flex justify-center items-center flex-col'>
+              <div className='w-[15vw] flex justify-center items-center flex-col'>
                 <span className='h-[6vw] w-[6vw] flex items-center justify-center rounded-full bg-arrow text-[1.5vw]'><i className="fa-solid fa-mobile-screen"></i></span>
                 <p className='text-[1.2vw]'>ScreenTime</p>
               </div>
 
-              <div className=' flex justify-center items-center flex-col'>
+              <div className='w-[15vw] flex justify-center items-center flex-col'>
                 <span className='h-[6vw] w-[6vw] flex items-center justify-center rounded-full bg-arrow text-[1.5vw]'><i className="fa-solid fa-user-shield"></i></span>
                 <p className='text-[1.2vw]'>Public or privet</p>
               </div>
